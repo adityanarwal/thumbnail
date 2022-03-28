d1361d96cff81e32875bd6f97a930c648a4e1012
adityanarwal <93639647+adityanarwal@users.noreply.github.com>
2022-03-28 14:06:31 +0530

    Update video_stream.py

--- program/video_stream.py
+++ program/video_stream.py
@@ -50,3 +50,153 @@ from pytgcalls.exceptions import (
     GroupCallNotFound,
 )
 from youtubesearchpython import VideosSearch
+
+
+def ytsearch(query: str):
+    try:
+        search = VideosSearch(query, limit=1).result()
+        data = search["result"][0]
+        songname = data["title"]
+        url = data["link"]
+        duration = data["duration"]
+        thumbnail = data["thumbnails"][0]["url"]
+        return [songname, url, duration, thumbnail]
+    except Exception as e:
+        print(e)
+        return 0
+
+async def ytdl(link):
+    proc = await asyncio.create_subprocess_exec(
+        "yt-dlp",
+        "--geo-bypass",
+        "-g",
+        "-f",
+        "[height<=?720][width<=?1280]",
+        f"{link}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await proc.communicate()
+    if stdout:
+        return 1, stdout.decode().split("\n")[0]
+    else:
+        return 0, stderr.decode()
+
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+async def play_tg_file(c: Client, m: Message, replied: Message = None, link: str = None):
+    chat_id = m.chat.id
+    user_id = m.from_user.id
+    if link:
+        try:
+            replied = await from_tg_get_msg(link)
+        except Exception as e:
+            LOGS.info(f"[ERROR]: {e}")
+            return await m.reply_text(f"🚫 error:\n\n» {e}")
+    if not replied:
+        return await m.reply(
+            "» reply to an **audio file** or **give something to search.**"
+        )
+    if replied.video or replied.document:
+        if not link:
+            loser = await replied.reply("📥 downloading video...")
+        else:
+            loser = await m.reply("📥 downloading video...")
+        dl = await replied.download()
+        link = replied.link
+        songname = "video"
+        duration = "00:00"
+        Q = 720
+        pq = m.text.split(None, 1)
+        if ("t.me" not in m.text) and len(pq) > 1:
+            pq = pq[1]
+            if pq == "720" or pq == "480" or pq == "360":
+                Q = int(pq)
+            else:
+                await loser.edit(
+                    "start streaming the local video in 720p quality"
+                )
+        try:
+            if replied.video:
+                songname = replied.video.file_name[:80]
+                duration = convert_seconds(replied.video.duration)
+            elif replied.document:
+                songname = replied.document.file_name[:80]
+        except BaseException:
+            songname = "video"
+
+        if chat_id in QUEUE:
+            await loser.edit("🔄 Queueing Track...")
+            gcname = m.chat.title
+            ctitle = await CHAT_TITLE(gcname)
+            title = songname
+            userid = m.from_user.id
+            thumbnail = f"{IMG_5}"
+            image = await thumb(thumbnail, title, userid, ctitle)
+            pos = add_to_queue(chat_id, songname, dl, link, "video", Q)
+            await loser.delete()
+            requester = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
+            buttons = stream_markup(user_id)
+            await m.reply_photo(
+                photo=image,
+                reply_markup=InlineKeyboardMarkup(buttons),
+                caption=f"💡 **Track added to queue »** `{pos}`\n\n"
+                        f"🗂 **Name:** [{songname}]({link}) | `video`\n"
+                        f"⏱️ **Duration:** `{duration}`\n"
+                        f"🧸 **Request by:** {requester}",
+            )
+            remove_if_exists(image)
+        else:
+            try:
+                await loser.edit("🔄 Joining Group Call...")
+                gcname = m.chat.title
+                ctitle = await CHAT_TITLE(gcname)
+                title = songname
+                userid = m.from_user.id
+                thumbnail = f"{IMG_5}"
+                image = await thumb(thumbnail, title, userid, ctitle)
+                if Q == 720:
+                    amaze = HighQualityVideo()
+                elif Q == 480:
+                    amaze = MediumQualityVideo()
+                elif Q == 360:
+                    amaze = LowQualityVideo()
+                await music_on(chat_id)
+                await add_active_chat(chat_id)
+                await calls.join_group_call(
+                    chat_id,
+                    AudioVideoPiped(
+                        dl,
+                        HighQualityAudio(),
+                        amaze,
+                    ),
+                    stream_type=StreamType().pulse_stream,
+                )
+                add_to_queue(chat_id, songname, dl, link, "video", Q)
+                await loser.delete()
+                requester = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
+                buttons = stream_markup(user_id)
+                await m.reply_photo(
+                    photo=image,
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                    caption=f"🗂 **Name:** [{songname}]({link}) | `video`\n"
+                            f"⏱️ **Duration:** `{duration}`\n"
+                            f"🧸 **Request by:** {requester}",
+                )
+                remove_if_exists(image)
+            except (NoActiveGroupCall, GroupCallNotFound):
+                await loser.delete()
+                await remove_active_chat(chat_id)
+                await m.reply_text("❌ The bot can't find the Group call or it's inactive.\n\n» Use /startvc command to turn on the Group call !")
+            except Exception as e:
+                LOGS.info(f"[ERROR]: {e}")
+    else:
+        await m.reply_text(
+            "» reply to an **video file** or **give something to search.**"
+        )
