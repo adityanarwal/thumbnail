0fdf25fb4f678ba90ecaf40867e622f86f073c2a
adityanarwal <93639647+adityanarwal@users.noreply.github.com>
2022-03-28 14:04:17 +0530

    Update video_stream.py

--- program/video_stream.py
+++ program/video_stream.py
@@ -1,1 +1,52 @@
+"""
+Video + Music Stream Telegram Bot
+Copyright (c) 2022-present levina=lab <https://github.com/levina-lab>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/licenses.html>
+"""
 
+
+import re
+import asyncio
+
+from asyncio.exceptions import TimeoutError
+from config import BOT_USERNAME, IMG_1, IMG_2, IMG_5
+
+from program import LOGS
+from program.utils.inline import stream_markup
+from driver.design.thumbnail import thumb
+from driver.design.chatname import CHAT_TITLE
+from driver.filters import command, other_filters
+from driver.queues import QUEUE, add_to_queue
+from driver.core import calls, user, me_user
+from driver.utils import remove_if_exists, from_tg_get_msg
+from driver.decorators import require_admin, check_blacklist
+from driver.database.dbqueue import add_active_chat, remove_active_chat, music_on
+
+from pyrogram import Client
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from pytgcalls import StreamType
+from pytgcalls.types.input_stream import AudioVideoPiped
+from pytgcalls.types.input_stream.quality import (
+    HighQualityAudio,
+    HighQualityVideo,
+    LowQualityVideo,
+    MediumQualityVideo,
+)
+from pytgcalls.exceptions import (
+    NoAudioSourceFound,
+    NoVideoSourceFound,
+    NoActiveGroupCall,
+    GroupCallNotFound,
+)
+from youtubesearchpython import VideosSearch
