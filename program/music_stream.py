50c7975c4a55763fa28386ab836ed860f89afb16
adityanarwal <93639647+adityanarwal@users.noreply.github.com>
2022-04-11 17:04:15 +0530

    Update music_stream.py

--- program/music_stream.py
+++ program/music_stream.py
@@ -350,7 +350,7 @@ async def audio_stream(c: Client, m: Message):
                         remove_if_exists(image)
                     else:
                         try:
-                            await suhu.edit("**Downloaded Successfully !!**\n\n**0% ■■■■■■■■■■ 100%**\n\n**Duration** :- 00:00 MbPs\n\n**Connecting To The Server....**")
+                            await suhu.edit("**Downloaded Successfully !!**\n\n**0% ■■■■■■■■■■ 100%**\n\nDuration :- 00:00 MbPs\n\nConnecting To The Server....")
                             await music_on(chat_id)
                             await add_active_chat(chat_id)
                             await calls.join_group_call(
@@ -510,7 +510,7 @@ async def live_music_stream(c: Client, m: Message):
                     remove_if_exists(image)
                 else:
                     try:
-                        await msg.edit_text("**Downloaded Successfully !!**\n\n**0% ■■■■■■■■■■ 100%**\n\n**Duration** :- 00:00 MbPs\n\n**Connecting To The Server....**")
+                        await msg.edit_text("**Downloaded Successfully !!**\n\n**0% ■■■■■■■■■■ 100%**\n\nDuration :- 00:00 MbPs\n\nConnecting To The Server....")
                         await music_on(chat_id)
                         await add_active_chat(chat_id)
                         await calls.join_group_call(
